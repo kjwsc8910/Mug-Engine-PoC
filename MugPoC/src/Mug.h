c2adf5a61675d56db37e7e8092b91723939e411f
KJ <05kj@sky.com>
2024-10-03 21:50:04 +0100

    Added Logging
    Added Logging to the Engine usable with Macros

--- MugPoC/src/Mug.h
+++ MugPoC/src/Mug.h
@@ -3,6 +3,7 @@
 // For use by Mug Applications
 
 #include "Mug/Application.h"
+#include "Mug/Log.h"
 
 // ---Entry Point---------------
 #include "Mug/EntryPoint.h"
--- MugPoC/src/Mug/EntryPoint.h
+++ MugPoC/src/Mug/EntryPoint.h
@@ -6,6 +6,11 @@ extern Mug::Application* Mug::CreateApplication();
 
 int  main(int argc, char** argv) {
 
+	Mug::Log::Init();
+	MG_CORE_WARN("Initialised Log!");
+	int a = 5;
+	MG_INFO("Hello! Var={0}", a);
+
 	auto app = Mug::CreateApplication();
 	app->Run();
 	delete app;
