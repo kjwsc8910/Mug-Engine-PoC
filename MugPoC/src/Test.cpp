3c9d899a56ae1c1e9b3f586de13dea4864bdcf3f
KJ <05kj@sky.com>
2024-10-03 19:07:40 +0100

    Initial Project Setup
    An initial project set up, capable of printing Hello World!

---
+++ MugPoC/src/Test.cpp
@@ -0,0 +1,13 @@
+#include "Test.h"
+#include <stdio.h>
+
+
+namespace Mug {
+
+	void Print() {
+
+		printf("Hello World!\n");
+
+	}
+
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,13 @@
+
+
+namespace Mug {
+
+	__declspec(dllimport) void Print();
+
+}
+
+void main() {
+
+	Mug::Print();
+
+}
