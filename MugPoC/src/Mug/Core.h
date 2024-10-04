08480670a18b43304f506bcf3777b5a71099dd17
KJ <05kj@sky.com>
2024-10-04 03:50:56 +0100

    Started Adding Event Handling
    Created Event Header files

--- MugPoC/src/Mug/Core.h
+++ MugPoC/src/Mug/Core.h
@@ -9,3 +9,5 @@
 #else
 	#error Mug only supports windows!
 #endif
+
+#define BIT(x) (1 << x)
---
+++ MugPoC/src/Mug/Events/Event.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "../Core.h"
+
+#include <string>
+#include <functional>
+
+namespace Mug {
+
+	// Event in Mug are currently Blocking.
+
+	enum class EventType {
+		none = 0,
+		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		AppTick, AppUpdate, AppRender,
+		KeyPressed, KeyReleased,
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
+
+	enum EventCatagory {
+		none = 0,
+		EventCatagoryApplication	= BIT(0),
+		EventCatagoryInput			= BIT(1),
+		EventCatagoryKeyboard		= BIT(2),
+		EventCatagoryMouse			= BIT(3),
+		EventCatagoryMouseButton	= BIT(4)
+	};
+
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+								virtual EventType GetEventType() const override { return GetStaticType(); }\
+								virtual const char* GetName() const override { return #type; }
+
+#define EVENT_CLASS_CATAGORY(catagory) virtual int GetCatagoryFlags() const override { return catagory; }
+
+	class MUG_API Event {
+		friend class EventDispatcher;
+	public:
+		virtual EventType GetEventType() const = 0;
+		virtual const char* GetName() const = 0;
+		virtual int GetCatagoryFlags() const = 0;
+		virtual std::string ToString() const { return GetName(); }
+
+		inline bool IsInCatagory(EventCatagory catagory) {
+			return GetCatagoryFlags() & catagory;
+		}
+	protected:
+		bool m_Handled = false;
+	};
+
+	class MUG_API EventDispatcher {
+		template<typename T>
+		using EventFn = std::function<bool(T&)>;
+	public:
+		EventDispatcher(Event& event)
+			: m_Event(event) {}
+
+		template<typename T>
+		bool Dispatch(EventFn<T> func) {
+			if (m_Event.GetEventType() == T::GetStaticType()) {
+				m_Event.m_Handled = func(*(T*)&m_Event);
+				return true;
+			}
+			return false;
+		}
+	private:
+		Event& m_Event;
+	};
+
+}
